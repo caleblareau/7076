43c99135b5e10408f8420f6e6f31c775ce1ea4c2
caleblareau <clareau@stanford.edu>
2022-02-08 00:24:15 -0800

    reference analyses

--- reference_genome/code/04_trna_mutation_landscape.R
+++ reference_genome/code/04_trna_mutation_landscape.R
@@ -2,9 +2,16 @@ library(data.table)
 library(dplyr)
 library(readxl)
 library(BuenColors)
-source("01_functions.R")
+library(VariantAnnotation)
 
-# Import Phewas associations
+
+# Now import tRNA annoations
+trna_positions_wide <- read.table("../../reference_genome/data/mt-universal-trna-genome-coordinates.csv", sep = ",", header = TRUE) 
+trna_positions_wide %>%
+  reshape2::melt(id.vars = "Position") %>%
+  filter(!is.na(value)) -> trna_positions
+
+# Import cancer associations
 impact <- fread("../../public_mutations/data/cancer/data_mutations_impact.txt.gz") %>%
   filter(Variant_Type == "SNP") %>%
   mutate(mutation = ShortVariantID)
@@ -22,12 +29,6 @@ data.frame(
   position = c(impact$Start_Position, tcga$Start_Position, pcawg$Start_Position)
 ) %>%
   group_by(mutation,position) %>% summarize(count_tumors = n()) -> cancer_count_df
-
-# Now import tRNA annoations
-trna_positions_wide <- read.table("../../reference_genome/data/mt-universal-trna-genome-coordinates.csv", sep = ",", header = TRUE) 
-trna_positions_wide %>%
-  reshape2::melt(id.vars = "Position") %>%
-  filter(!is.na(value)) -> trna_positions
 
 # Do analysis of tcga
 trna_positions %>%
@@ -49,8 +50,6 @@ mdf %>% group_by(Position) %>%
   summarize(sum = sum(count_tumors)) %>%
   arrange(as.numeric(as.character(Position))) %>% data.frame()
 
-
-
 # Now look at mitomap
 rna <- fread("../data/mitomap-RNA-mutations.txt")
 positions_rna_all <- rna[complete.cases(rna),"V1"][[1]]
@@ -68,3 +67,28 @@ trna_positions %>% group_by(Position) %>%
   scale_color_manual(values = c("dodgerblue3", "firebrick")) +
   labs(x = "Position in reference tRNA", y = "# of mutation events", "mutation type") +
   pretty_plot() + L_border() + theme(legend.position = "bottom")
+
+# Now look at gnomad
+vcf <- readVcf( "../data/gnomad.genomes.v3.1.sites.chrM.vcf.bgz", verbose = FALSE )
+
+simple_gnomad <- data.frame(rowRanges(vcf)@ranges,
+                            n_homo = vcf@info$AC_hom) %>% filter(width == 1)
+simple_gnomad$id <- paste0("m", simple_gnomad$start)
+trna_positions$id <- paste0("m", trna_positions$value)
+
+mdf <- merge(simple_gnomad, trna_positions, by = "id")
+mdf2 <- mdf %>% group_by(Position, variable) %>%
+  summarize(n_homo = sum(n_homo))
+ggplot(mdf2, aes(x = as.numeric(as.character(Position)), y = variable, fill = log10(n_homo + 1), label = n_homo)) +
+  geom_tile(color = "black") +geom_text() + scale_fill_gradientn(colors = jdb_palette("brewer_heat")) + pretty_plot() + L_border() +
+  theme(legend.position = "bottom") + labs(x = "Position in reference tRNA", y = 'Specific tRNA', color = "log10 n_homoplasmic") 
+
+mdf %>% group_by(Position) %>%
+  summarize(sum = sum(n_homo)) %>%
+  ggplot(aes(x = as.numeric(as.character(Position)), y = sum)) +
+  geom_point() + geom_line() +
+  pretty_plot() + L_border() + labs(x = "Position in reference mt tRNA", y = "Total number of mutations observed") +
+  ggtitle("gnomad")
+
+mdf %>% group_by(variable) %>%
+  summarize(total = sum(n_homo)) %>% arrange(desc(total)) %>% data.frame()
